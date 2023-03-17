bb29467de774ce1ca05c52845501df2ea44a2635
Johana Ringelová <ringelovaj@gmail.com>
2023-03-17 15:30:03 +0100

    Řešení cv. 3_minutka

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('funguju!');
+let ring = () => {
+    let alarm = document.querySelector(".alarm")
+    alarm.classList.add("alarm--ring")
+}
+
+let time = prompt("Zadejte, za kolik vteřin má budík začít zvonit.") * 1000
+
+
+let playAudio = () => {
+    let audio = document.querySelector(".audio")
+    audio.play()
+
+}
+
+let timeout = setTimeout(function(){
+    playAudio()
+    ring()
+}, time)
+
+let btn = document.createElement("button");
+btn.innerHTML = "Stop the alarm";
+document.querySelector(".container").appendChild(btn);
+btn.addEventListener("click", function onclick(){
+    clearTimeout(timeout)
+})  
+
+
+
+
+
+
+
+/*
+    Přidejte na stránku tlačítko, které umožní odpočet minutky zrušit dřív, než začne zvonit.
+
+    */
+
